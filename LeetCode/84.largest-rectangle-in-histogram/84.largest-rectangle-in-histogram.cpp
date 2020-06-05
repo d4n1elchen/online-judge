f3527c190377d7be303adf2b135e8c1d2cb738ea
Daniel Chen <team6612@gmail.com>
2020-06-06 06:51:10 +0800

    Yo

---
+++ LeetCode/84.largest-rectangle-in-histogram/84.largest-rectangle-in-histogram.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=84 lang=cpp
+ *
+ * [84] Largest Rectangle in Histogram
+ *
+ * https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+ *
+ * algorithms
+ * Hard (34.32%)
+ * Likes:    3370
+ * Dislikes: 78
+ * Total Accepted:    250.4K
+ * Total Submissions: 728.7K
+ * Testcase Example:  '[2,1,5,6,2,3]'
+ *
+ * Given n non-negative integers representing the histogram's bar height where
+ * the width of each bar is 1, find the area of largest rectangle in the
+ * histogram.
+ * 
+ * 
+ * 
+ * 
+ * Above is a histogram where width of each bar is 1, given height =
+ * [2,1,5,6,2,3].
+ * 
+ * 
+ * 
+ * 
+ * The largest rectangle is shown in the shaded area, which has area = 10
+ * unit.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [2,1,5,6,2,3]
+ * Output: 10
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        
+    }
+};
+// @lc code=end
